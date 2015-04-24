4001424e269da761a181f58120ea24b4544c8ae2
zbr80259 <zbr80259@alibaba-inc.com>
2015-04-24 12:38:26 +0800

    support compress option

--- changess-grunt/tasks/changess.js
+++ changess-grunt/tasks/changess.js
@@ -3,19 +3,23 @@ var fs=require('fs'),path=require('path');
 module.exports=function(grunt){
   var interpreter=require('./lib/changess'),libSheets=interpreter.parse(readSrcLib());
   grunt.registerMultiTask('changess','complie Changess Files to css',function(){
-   var opt=this.options({keepEmptyResult:false});
+    var opt=this.options(
+      {
+        keepEmptyResult:false,
+        compress:false
+      });
     interpreter.opt.keepEmptyResult=opt.keepEmptyResult;
+    opt.lib=libSheets;
     this.files.forEach(function(f){
       var contents=[];
       f.src.forEach(function(filepath){
         if (!grunt.file.exists(filepath))
           grunt.log.warn('Source file "' + filepath + '" not found.');
         else {
-          //grunt.log.writeln(filepath);
           contents.push(grunt.file.read(filepath));
         }
       });
-      grunt.file.write(f.dest,interpreter(contents.join(''),{lib:libSheets}));
+      grunt.file.write(f.dest,interpreter(contents.join(''),opt));
       grunt.log.writeln('File ' + f.dest + ' created.');
     });
   });
